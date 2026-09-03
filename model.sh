7de028f7fbcb16afe6e6b0c809516c81b8711f8b
liam-tolbert <liam.tolbert02@gmail.com>
2026-09-02 20:53:24 -0400

    huge claude refactor of redundant/dead code and technical debt resolutions

--- model.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Resolve script location to make it portable
-DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
-# Pass all arguments ($@) to the Python script
-python "$DIR/src/predict.py" "$@"
